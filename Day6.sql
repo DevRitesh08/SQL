6e127062c45b51869936e7a1be4596a1380cea28
ritesh swami <riteshswamiofficial@gmail.com>
2025-07-31 16:12:28 +0530

    Implement code changes to enhance functionality and improve performance

--- Day6.sql
+++ Day6.sql
@@ -84,3 +84,123 @@ select * from student
 UNION ALL
 select * from student2;
 
+
+
+--- Case 1 - Failed
+
+select stu_id, `name` from student
+UNION
+select `name`, stu_id from student2;
+
+--- Case 2 - Not Failed
+select stu_id, name from student
+UNION
+select stu_id, name from student2;
+
+--- Case 3 - Not Failed
+select stu_id as stu_id_college_1 , name from student
+UNION
+select stu_id as stu_id_college_2, name from student2;
+
+--- Case 4 - Failed
+select stu_id from student
+UNION
+select email from student2;
+
+--- Case 5 - Failed
+select stu_id , name from student
+UNION
+select email from student2;
+
+
+
+-- Common Table Expressions (CTE)
+
+create table amazon_employees(
+    emp_id int,
+    emp_name varchar(20),
+    dept_id int,
+    salary int
+
+ );
+
+ insert into amazon_employees values(1,'Shashank', 100, 10000);
+ insert into amazon_employees values(2,'Rahul', 100, 20000);
+ insert into amazon_employees values(3,'Amit', 101, 15000);
+ insert into amazon_employees values(4,'Mohit', 101, 17000);
+ insert into amazon_employees values(5,'Nikhil', 102, 30000);
+
+ create table department
+ (
+    dept_id int,
+    dept_name varchar(20) 
+  );
+
+ insert into department values(100, 'Software');
+insert into department values(101, 'HR');
+insert into department values(102, 'IT');
+insert into department values(103, 'Finance');
+
+--- Write a query to print the name of department along with the total salary paid in each department
+--- Normal approach
+select d.dept_name, tmp.total_salary
+from (select dept_id , sum(salary) as total_salary from amazon_employees group by dept_id) tmp
+inner join department d on tmp.dept_id = d.dept_id;
+
+--- how to do it using with clause??
+with dept_wise_salary as (select dept_id , sum(salary) as total_salary from amazon_employees group by dept_id)
+select d.dept_name, tmp.total_salary
+from dept_wise_salary tmp
+inner join department d on tmp.dept_id = d.dept_id;
+
+SELECT * FROM dept_wise_salary;
+-- CTE can be used to simplify complex queries and make them more readable , it has no existence after the query .
+
+-- with is faster than subquery and views because it is executed only once and the result is stored in memory, whereas subqueries and views are executed every time they are called in the query.
+
+
+--- Write a Query to generate numbers from 1 to 10 in SQL
+
+with recursive generate_numbers as   
+(
+  select 1 as n
+  union 
+  select n+1 from generate_numbers where n<10
+) 
+select * from generate_numbers;
+
+
+create table emp_mgr
+(
+id int,
+name varchar(50),
+manager_id int,
+designation varchar(50),
+primary key (id)
+);
+
+
+insert into emp_mgr values(1,'Shripath',null,'CEO');
+insert into emp_mgr values(2,'Satya',5,'SDE');
+insert into emp_mgr values(3,'Jia',5,'DA');
+insert into emp_mgr values(4,'David',5,'DS');
+insert into emp_mgr values(5,'Michael',7,'Manager');
+insert into emp_mgr values(6,'Arvind',7,'Architect');
+insert into emp_mgr values(7,'Asha',1,'CTO');
+insert into emp_mgr values(8,'Maryam',1,'Manager');
+
+
+select * from emp_mgr;
+
+--- for our CTO 'Asha', present her org chart
+
+with recursive emp_hir as  
+(
+   select id, name, manager_id, designation from emp_mgr where name='Asha'
+   UNION
+   select em.id, em.name, em.manager_id, em.designation from emp_hir eh inner join emp_mgr em on eh.id = em.manager_id
+)
+
+select * from emp_hir;
+
+--- Print level of employees as well
