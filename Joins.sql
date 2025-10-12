f6dd7978a69b8c4b273bcc4f1924b282b5d7b61e
ritesh swami <riteshswamiofficial@gmail.com>
2025-10-12 15:28:42 +0530

    Add documentation for SQL joins in Joins.sql and create a new markdown file for Joins

---
+++ Joins.sql
@@ -0,0 +1,13 @@
+----
+-- Joins
+----
+
+-- In SQL (Structured Query Language), a JOIN is a way to combine data from two or more database tables
+-- based on a related column between them .
+
+-- Joins are used when we want to query information that is distributed across multiple tables in a database ,
+-- and the information we need is not contained in a single table .
+-- By joining tables together , we can create a virtual table that contains all of the information we need for our query .
+
+-- To perform a JOIN , it is necessary to have a common column between the tables being joined .
+
