6e9c36815954f5ce417c78c3239aa19b8153c097
Ritesh swami <riteshswamiofficial@gmail.com>
2026-08-15 15:40:20 +0530

    day 1 finished

--- Day1.sql
+++ Day1.sql
@@ -14,14 +14,22 @@ create table salesman(
     salesmanname VARCHAR(50)
 );
 
+
 CREATE TABLE if not exists Customer(
-    CustomerID INT PRIMARY KEY,
+    CustomerID INT --PRIMARY KEY
+    ,
     CustomerName VARCHAR(50),
     LastName VARCHAR(50),
     Country VARCHAR(50),
-    Age INT CHECK (Age >= 0 AND Age <= 99),
+    Age INT --CHECK (Age >= 0 AND Age <= 99)
+    ,
     Phone int(10)
+
+    -- adding constraints to the table ==> better way to add constraints is to add them after the column definition and give them a name so that we can identify them in case of errors and also we can drop them easily if required.
+    constraint Age_check check (Age >= 0 AND Age <= 99)
+    constraint pk_CustomerID PRIMARY KEY (CustomerID)
 );
+
 
 --command to list tables
 show TABLES ;
@@ -32,42 +40,55 @@ describe Customer;
 -- command to drop the table
 drop table if exists salesman;
 
--- to get the schema or defination of the table
+-- To get the schema or defination of the table
 show create table Customer;
+-- why create table is used ? => because it shows the complete definition of the table including the constraints and their names.
 
 
 
 -- command to insert data in tables
 --Method 1 : when we only specify the table name then we have to insert values for all columns in the same order as they are defined in the table .
-insert into Customer values(1, 'Shubham', 'Thakur', 'India','23','435356353'),
-       (2, 'Aman ', 'Chopra', 'Australia','21','435356353'),
-       (3, 'Naveen', 'Tulasi', 'Sri lanka','24','435356353');
+insert into Customer values(1, 'Shubh', 'Chopra', 'India','23','435356353'),
+       (2, 'ema ', 'smith', 'Australia','21','435356353'),
+       (3, 'Naval', 'Thakur', 'Sri lanka','24','435356353');
 
 -- if we skip any column then the code will throw an error
 insert into Customer values(4, 'Amit', 'Kumar', 'India',25);
 
 --Method 2 : when we specify the column names then we can insert values for only those columns and the rest will be set to default or NULL.
-INSERT INTO Customer (CustomerID, CustomerName, LastName, Country, Age, Phone)
-VALUES (1, 'Shubham', 'Thakur', 'India','23','435356353'),
-       (2, 'Aman ', 'Chopra', 'Australia','21','435356353'),
-       (3, 'Naveen', 'Tulasi', 'Sri lanka','24','435356353'),
-       (4, 'Aditya', 'Arpan', 'Austria','21','435356353'),
-       (5, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','22','435356353');
+INSERT INTO Customer (CustomerID, CustomerName, LastName, Country, Phone)
+VALUES (4, 'Shubh', 'Thakur', 'India','435356353'),
+       (5, 'ema ', 'Chopra', 'Australia','435356353'),
+       (6, 'sam', 'Tulasi', 'Sri lanka','435356353'),
+       (7, 'Aditya', 'Arpan', 'Austria','435356353'),
+       (8, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','435356353');
        
 
 
 
 -- use select to view the data in the table .
-SELECT* FROM Customer;
+SELECT * FROM Customer;
+
+
+
+
+-- COUNT(*) VS COUNT(column_name) => COUNT(*) counts all rows in the table including NULL values whereas COUNT(column_name) counts only the rows where the column value is not NULL.
+
+select count(*) from Customer;
+select count(age) from Customer;
+
+select * from Customer where age is null;
+
+select * from Customer where age > 0;       -- this also can skip the null values because null is not greater than 0 it is simply unknown value so it will not be included in the result set.
+
+-- "<>" means not equal to in SQL. It is used to filter the data based on a condition where the value of a column is not equal to a specified value.
+select * from Customer where age <> 21; -- skip null and  21 .
 
 
 
 
 
-
-
-
--- EXAMPLE  for integrity constraint
+-- EXAMPLE  for integrity constraint 
 CREATE TABLE IF NOT EXISTS Orders (
     OrderID INT ,
     CustomerID INT NOT NULL,
@@ -75,6 +96,20 @@ CREATE TABLE IF NOT EXISTS Orders (
     UNIQUE(OrderID),
     check(OrderID > 5)
 ) ;
+
+-- better way to add constraints is to add them after the column definition and give them a name so that we can identify them in case of errors and also we can drop them easily if required.
+
+
+-- CREATE TABLE IF NOT EXISTS Orders (
+--     OrderID INT ,
+--     CustomerID INT NOT NULL ,
+--     OrderDate DATE DEFAULT '2025-07-16',
+
+--     constraint UNIQUE_orderid UNIQUE(OrderID),
+--     constraint orderId_check check(OrderID > 5)
+-- ) ;
+
+
 
 -- error will be thrown if we try to insert a value for OrderID that is less than or equal to 5 .
 INSERT into orders values(3, 1, '2025-07-16');
---
+++ SQL Revise/practise.sql
@@ -0,0 +1,22 @@
+show databases;
+
+use test;
+
+CREATE TABLE IF NOT EXISTS Orders (
+    OrderID INT ,
+    CustomerID INT NOT NULL,
+    OrderDate DATE DEFAULT '2025-07-16',
+
+    constraint UNIQUE_orderid UNIQUE(OrderID),
+    constraint orderId_check check(OrderID > 5)
+);
+
+insert into orders values
+(30, 8, '2025-07-16'),
+(30, 9, '2025-07-16');
+
+drop table IF EXISTS Orders;
+
+
+
+
